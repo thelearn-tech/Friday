9e5c5e9fbf85ddf31de025b8be9d048d1477cc93
Pritam Behera <69230350+thelearn-tech@users.noreply.github.com>
2021-01-27 12:24:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,14 @@
-printf '\033[36m';Friday installation script
-echo made by thelearn-tech 
+printf '\033[36m';echo Friday installation script
+echo made by thelearn-tech
+sleep 3
 pkg install espeak -y
 pkg install curl -y
 pkg install termux-api -y
 pkg install neofetch
 pkg install wget -y
-chmod +x friday
+chmod +x friday                                                                         clear
 cp friday /data/data/com.termux/files/usr/bin
-cd Friday.exc
-touch weather
-printf '\033[36m';echo "Enter city name like 'Kolkata' ,
-echo make sure first letter is in capital
-sleep 2
-read a
-echo "curl -s wttr.in/$a | head -7" >> weather
-chmod +x weather
-cd ..
-cp Friday.exc /data/data/com.termux/files/usr/var
+cp -r Friday.exc /data/data/com.termux/files/usr/var
 echo ""
 sleep 3
 echo thanks for downloading Friday
