a4c804c0998e9b32aacf528b7f478751eb442045
Pritam Behera <69230350+thelearn-tech@users.noreply.github.com>
2021-11-03 12:50:38 +0530

    Delete .Friday directory

--- .Friday/noti.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-t=$(cat timer_set_time.txt)
-
-termux-notification --title "Time Over" --content "Your set timer of $t seconds is over" \
- --button1 "remove" --button1-action "termux-notification-remove 346969" --id 346969 | termux-vibrate 
-
